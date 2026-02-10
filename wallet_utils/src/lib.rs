8047f3e20ab0f7b93c3b1205326842363a57976f
nikhil kumar <nikhilkumarpps@gmail.com>
2026-02-10 11:36:28 +0530

    ft: adding send tx fn to wallet utils

--- wallet_utils/src/lib.rs
+++ wallet_utils/src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use ethers::{
+    providers::{Http, Middleware, Provider},
+    signers::{LocalWallet},
+    types::{Address, TransactionRequest, U256, H256}
+};
+use std::{sync::Arc};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub async fn send_transaction(
+    rpc_url: &str,
+    private_key: &str,
+    to: Address,
+    value: U256,
+    data: Option<Vec<u8>>
+) -> Result<H256, Box<dyn std::error::Error>> {
+    // provider
+    let provider = Provider::<Http>::try_from(rpc_url).unwrap();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    // walet
+    let wallet: LocalWallet = private_key.parse().unwrap();
+
+    let client = Arc::new(
+        ethers::middleware::SignerMiddleware::new(
+            provider,
+            wallet
+        )
+    );
+
+    let mut tx = TransactionRequest::new().to(to).value(value);
+    if let Some(d) = data {
+        tx = tx.data(d);
     }
+
+    let pending = client.send_transaction(tx, None).await?;
+
+    let receipt = pending.await?;
+
+    let receipt = receipt.ok_or_else(|| anyhow::anyhow!("tx dropped"))?;
+
+    Ok(receipt.transaction_hash)
 }
